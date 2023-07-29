164e352e3af3a10c1631298fe8c5f6b43b0553f3
Nyanchama Brendah <nyanchamabrendah3@gmail.com>
2023-07-29 20:04:38 +0300

    admin_dashboard

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import { ColorModeContext,useMode } from './theme';
+import { Routes,BrowserRouter, Route,Link } from 'react-router-dom';
+import { CssBaseline,ThemeProvider } from '@mui/material';
+import Topbar from './scenes/global/Topbar';
+import Dashboard from './scenes/dashboard';
+import Sidebar from './scenes/global/Sidebar';
+import { useState } from 'react';
 
+import Students from './scenes/students';
+import Teachers from './scenes/teachers';
+import Events from './scenes/calendar';
+import Accounts from './scenes/accounts';
+import Pie from './scenes/pie';
+import Line from './scenes/line';
+import Bar from './scenes/bar';
+import FAQS from './scenes/faqs';
+import Notifications from './scenes/notifications';
+import Exams from './scenes/exams';
+ import Profile  from './scenes/profile';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [theme,colorMode] = useMode();
+  const [isSidebar, setIsSidebar] = useState(true);
+
+
+
+  return (<ColorModeContext.Provider value={colorMode}>
+     <ThemeProvider theme={theme}>
+      <CssBaseline/>
+      <div className="app">
+          <Sidebar isSidebar={isSidebar} />
+          <main className="content">
+            <Topbar setIsSidebar={setIsSidebar} />
+            
+        <Routes>
+        <Route path="/" element = {<Dashboard/>} />
+       <Route path="/students" element = {<Students/>} />
+       <Route path="/teachers" element = {<Teachers/>} />
+        <Route path="/calendar" element = {<Events/>} />
+        <Route path="/accounts" element = {<Accounts/>} />
+        <Route path="/exams" element = {<Exams/>} />
+        <Route path="/pie" element = {<Pie/>} />
+        <Route path="/line" element = {<Line/>} />
+        <Route path="/bar" element = {<Bar/>} />
+        <Route path="/faqs" element = {<FAQS/>} />
+        <Route path="/profile" element={<Profile/>} />
+  <Route path="/notifications" element = {<Notifications/>} />*/
+        </Routes>
+        
+        </main>
+     </div>
+     </ThemeProvider>
+      </ColorModeContext.Provider>); 
+ 
 }
 
 export default App;
